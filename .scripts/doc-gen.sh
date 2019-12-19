8600e5daff8e28e15d1dad5fdc34ce294a9b57e6
Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>
2019-12-19 09:48:07 -0700

    update docgen to use 0.12 compatible binary

--- .scripts/doc-gen.sh
+++ .scripts/doc-gen.sh
@@ -2,60 +2,53 @@
 
 set -e
 
+NAME="Terraform Enterprise: Clustering"
 BINARY_DIR=./work
-BINARY_FILE="${BINARY_DIR}/terraform-docs"
-BINARY_VERSION=0.6.0
-BINARY_URL_PREFIX="https://github.com/segmentio/terraform-docs/releases/download/v${BINARY_VERSION}/terraform-docs-v${BINARY_VERSION}"
+BINARY_FILE="${BINARY_DIR}/terraform-ci-docgen"
+BINARY_VERSION=0.0.2
+BINARY_URL_PREFIX="https://github.com/erindatkinson/terraform-ci-docgen/releases/download/v${BINARY_VERSION}/terraform-ci-docgen_${BINARY_VERSION}"
 
-DOCS_CMDS="--sort-inputs-by-required --with-aggregate-type-defaults markdown table"
+DOCS_CMDS="--sortRequiredFirst --moduleName"
+
 DOCS_DIR=docs
-
-VARS_TF=variables.tf
-OUTS_TF=outputs.tf
-
-INS_MD=inputs.md
-OUTS_MD=outputs.md
+DOC_MD=module.md
 
 
 function setup {
     mkdir -p ${BINARY_DIR}
     if [[ ! -e "${BINARY_FILE}" ]]; then
         if [[ "$OSTYPE" == "linux-gnu" ]]; then
-            BINARY_URL="${BINARY_URL_PREFIX}-linux-amd64"
+            BINARY_URL="${BINARY_URL_PREFIX}_linux_amd64.zip"
         elif [[ "$OSTYPE" == "darwin"* ]]; then
-            BINARY_URL="${BINARY_URL_PREFIX}-darwin-amd64"
+            BINARY_URL="${BINARY_URL_PREFIX}_darwin_amd64.zip"
         else
             echo "Please run this in either a Linux or Mac environment."     
             exit 1  
         fi
         echo "Downloading ${BINARY_URL}"
-        curl -L -o "${BINARY_FILE}" "${BINARY_URL}"
+        curl -L -o "${BINARY_FILE}.zip" "${BINARY_URL}"
+        unzip "${BINARY_FILE}.zip" -d "${BINARY_DIR}"
         chmod +x "${BINARY_FILE}"
     fi
 }
 
 function main_docs {
+    echo "Writing main docs"
     if test ! -d "${DOCS_DIR}"; then
         mkdir "${DOCS_DIR}"
     fi
 
-    echo -e "# Terraform Enterprise: Clustering\n" | tee "${DOCS_DIR}/${INS_MD}" "${DOCS_DIR}/${OUTS_MD}" &> /dev/null
-
-    eval "${BINARY_FILE} ${DOCS_CMDS} ${VARS_TF}"  >> "${DOCS_DIR}/${INS_MD}"
-    eval "${BINARY_FILE} ${DOCS_CMDS} ${OUTS_TF}" >> "${DOCS_DIR}/${OUTS_MD}"
-
+    eval "${BINARY_FILE} ${DOCS_CMDS} '${NAME}'"  > "${DOCS_DIR}/${DOC_MD}"
 }
 
 
 function module_docs {
+    echo "Writing module docs"
     if test -d ./modules; then
         for dir in ./modules/*; do
             mkdir -p "${dir}/${DOCS_DIR}"
-            
-            echo -e "# Terraform Enterprise: Clustering\n" | tee "${dir}/${DOCS_DIR}/${INS_MD}" "${dir}/${DOCS_DIR}/${OUTS_MD}" &> /dev/null
-
-            eval "${BINARY_FILE} ${DOCS_CMDS} ${dir}/${VARS_TF}"  >> "${dir}/${DOCS_DIR}/${INS_MD}"
-            eval "${BINARY_FILE} ${DOCS_CMDS} ${dir}/${OUTS_TF}" >> "${dir}/${DOCS_DIR}/${OUTS_MD}"
+            local base=$(basename "${dir}")
+            eval "${BINARY_FILE} ${DOCS_CMDS} '${NAME} - ${base}' ${dir}"  >> "${dir}/${DOCS_DIR}/${DOC_MD}"
         done
     else
         echo "No modules directory, skipping."
