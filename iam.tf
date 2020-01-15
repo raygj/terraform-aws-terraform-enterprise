59c037420704174c1f908e32ebe4e7a2e815f74a
Jim Ray <ray.jim@gmail.com>
2020-01-15 15:46:31 -0500

    tf01.12 fixes

--- iam.tf
+++ iam.tf
@@ -13,13 +13,13 @@ data "aws_iam_policy_document" "ptfe" {
   }
 }
 resource "aws_iam_role" "ptfe" {
-  name = "ptfe-${module.common.install_id}"
+  name = ptfe-module.common.install_id
 
   assume_role_policy = data.aws_iam_policy_document.ptfe.json
 }
 
 resource "aws_iam_instance_profile" "ptfe" {
-  name = "${var.prefix}-${module.common.install_id}"
+  name = var.prefix-module.common.install_id
   role = aws_iam_role.ptfe.name
 }
 
